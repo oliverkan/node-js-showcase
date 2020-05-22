90ffa1bc6a3d0bdaff379f49301c7ccdefdcd66c
oliverkan <erkancaglartaskin@gmail.com>
2020-05-22 19:12:37 +0100

    new method added to check countries are ready in db. if countries are not in db, read countries file and insert to mongo db

---
+++ data/dataOperations.js
@@ -0,0 +1,30 @@
+const Country = require('../models/country');
+const fs = require("fs");
+const path = require("path");
+
+exports.checkData = function () {
+    Country.find(function (err, countries) {
+        if (err) {
+            throw 'error on country query'
+        }
+        if (countries.length == 0) {
+            const countriesData = JSON.parse(fs.readFileSync(path.resolve("data", "countries.json")).toString());
+            //convert country array to Country document objects for mongoDb
+            const countriesObject = countriesData.map(c => {
+                return new Country(
+                    {
+                        name: c.name,
+                        code: c.code
+                    })
+            })
+
+            Country.insertMany(countriesObject, function (err) {
+                if (err) {
+                    throw 'error on country insert'
+                }
+                console.log('Countries inserted successfully')
+            })
+        }
+        console.log(countries.length + ' countries found in db');
+    })
+}
