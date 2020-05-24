94c24946b435779ffe8b1de337e2342629c9db39
oliverkan <erkancaglartaskin@gmail.com>
2020-05-24 01:04:07 +0100

    timestamps(createdAt and updatedAt) added to country model

--- models/country.js
+++ models/country.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 let CountrySchema = new Schema({
     name: {type: String, required: true, max: 100},
     code: {type: String, required: true, max: 100}
-});
+}, {timestamps: true});
 
 
 // Export the model
