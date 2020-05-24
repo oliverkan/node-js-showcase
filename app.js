745cab714561067b14fd77fd39e2ab1c27ec2e9c
oliverkan <erkancaglartaskin@gmail.com>
2020-05-24 01:06:16 +0100

    error handling removed

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const compression = require('compression');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dataOperations = require('./data/dataOperations');
 require("custom-env").env(true);
 
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
+const countriesRouter = require('./routes/countries');
 
 var app = express();
 
@@ -23,9 +24,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
+app.use('/countries', countriesRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
     next(createError(404));
 });
 
@@ -38,7 +40,7 @@ app.use(function (err, req, res, next) {
     // render the error page
     //res.status(err.status || 500);
     //res.render('error');
-});
+});*/
 
 // Set up mongoose connection
 
