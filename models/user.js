4c8c16ca80ea7d2ddd7e1d17e42309f83fb4723c
oliverkan <erkancaglartaskin@gmail.com>
2020-05-24 01:03:08 +0100

    new attributes added to user model

--- models/user.js
+++ models/user.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
-    name: {type: String, required: true, max: 100},
-    lastName: {type: String, required: true, max: 100},
-    birthDate: { type: Date, required:true },
-    nationality:  { type: mongoose.Schema.Types.ObjectId, ref: 'Country' }
-});
+        userName: {type: String, required: true, max: 64},
+        name: {type: String, required: true, max: 100},
+        lastName: {type: String, required: true, max: 100},
+        birthDate: {type: Date, required: true},
+        nationality: {type: mongoose.Schema.Types.ObjectId, ref: 'Country'},
+        password: {type: String, required: true}
+    },
+    {timestamps: true});
 
 
 // Export the model
